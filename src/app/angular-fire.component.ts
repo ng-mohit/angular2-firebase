19c747bfe8b459a94a69e68ec49247add40ba43f
Mohit <mohitmathur@zykoontech.com>
2016-06-16 19:17:12 +0530

    initial commit

--- src/app/angular-fire.component.ts
+++ src/app/angular-fire.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   moduleId: module.id,
@@ -8,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AngularFireAppComponent {
   title = 'angular-fire works!';
+  user= [];
+ items: FirebaseListObservable<any[]>;
+ 
+  //items: FirebaseObjectObservable<any[]>;
+  constructor(af: AngularFire) {
+    //this.items = af.database.object('/expenses');
+    this.items = af.database.list('/expenses');
+    //this.items.subscribe(snapshot => console.log(snapshot));
+    //this.itemObservable = af.database.object('/item');
+  }
+  
+  save(user){
+    console.log(user);
+    this.items.push(user);    
+  } 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AngularFireAppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AngularFireAppComponent);
+bootstrap(AngularFireAppComponent, [
+  FIREBASE_PROVIDERS,
+  defaultFirebase('https://glorious-expense.firebaseio.com/')
+]);
 
